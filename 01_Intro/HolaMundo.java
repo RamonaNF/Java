ec4d0f96c4820a1bace1b1e99b3bae21dea7a737
RamonaNF <a01423596@tec.mx>
2023-06-26 01:20:49 -0600

    feat: intro

---
+++ 01_Intro/HolaMundo.java
@@ -0,0 +1,43 @@
+
+// CLASS
+public class HolaMundo {
+    /*
+     * MÉTODOS
+     *   MAIN
+     */
+    public static void main(String[] args) { 
+        System.out.println("Hola Mundooo... desde Java");
+    }
+}
+
+/*
+ * CLASS
+ *   Todo en java es una clase 
+ *    EXCEPTO por los tipos primitivos (no son objetos, representan valor)
+ * 
+ *   Modificador de acceso
+ *     public
+ *       Se puede acceder/instanciar en cualquier parte de la aplicación
+ */
+
+/*
+ * MÉTODOS
+ *   Modificador de acceso
+ *     public
+ *       EL método puede ser llamado/ejecutado desde cualquier parte de la aplicación
+ * 
+ *     static
+ *       Propias de la clase
+ *       CONTRAPARTE: propias de la instancia
+ */
+
+/*
+ * MAIN
+ *   Puerta de entrada a la aplicación
+ *    que le permite ejecutarse como una aplicación autónoma
+ * 
+ *   Ejecutado por JVM (por eso debe ser un método público)
+ * 
+ *   String[] args
+ *     Parámetros de entrada (argumentos que se pasan al ejecutar la aplicación)
+ */
